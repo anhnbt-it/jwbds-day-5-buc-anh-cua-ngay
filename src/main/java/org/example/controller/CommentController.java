f1a5b1f124b89cb2f8ebbdb93cf91f78cb948e75
anhnbt-it <anhnbt.it@gmail.com>
2020-12-15 19:46:36 +0700

    feat: like using ajax

--- src/main/java/org/example/controller/CommentController.java
+++ src/main/java/org/example/controller/CommentController.java
@@ -22,7 +22,7 @@ public class CommentController {
     @GetMapping
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.addObject("comment", new Comment((short) 4, "@anhnbt", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."));
+        modelAndView.addObject("comment", new Comment((short) 4, "@anhnbt", "CodeGym MonCity"));
         List<Comment> comments = commentSvc.findAll();
         modelAndView.addObject("comments", comments);
         return modelAndView;
